35be78a8da869b5cb022ec5f1cf4e38fd6a9bc63
Oleg <oleg.rusnak04@gmail.com>
2025-05-19 18:29:52 +0300

    changed App to App.jsx in index.js and added some examples of dishes

---
+++ src/data.js
@@ -0,0 +1,8 @@
+// data.js
+export const categories = ['Сніданок', 'Обід', 'Вечеря', 'Десерт'];
+
+export const initialDishes = [
+  { id: 1, name: 'Омлет з сиром', category: 'Сніданок', ingredients: 'Яйця, сир, сіль', description: 'Смачний білковий сніданок' },
+  { id: 2, name: 'Борщ', category: 'Обід', ingredients: 'Буряк, капуста, м`ясо', description: 'Традиційна українська страва' },
+  { id: 3, name: 'Наполеон', category: 'Десерт', ingredients: 'Тісто, крем', description: 'Солодкий листковий торт' },
+];
